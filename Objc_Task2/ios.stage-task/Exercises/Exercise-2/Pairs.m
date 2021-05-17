00ae70b9db7c3c4326113848e1556144f11ba718
Oleksii Sur <oleksii_sur@epam.com>
2021-05-17 21:31:00 +0300

    feat: solutions for exercises 2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,23 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger count = 0;
+    
+    if (array.count == 0 || array.count == 1 ) {
+        return 0;
+    }
+    for (int n = 0; n < (array.count - 1); n++) {
+        NSInteger num = [[array objectAtIndex:n] intValue];
+        for (int k = n + 1; k < (array.count); k++) {
+            NSInteger num2 = [[array objectAtIndex: k] intValue];
+            if (num + [number intValue] == num2 ) {
+                count ++ ;
+            }
+        }
+    }
+
+    return count;
+    
 }
 
 @end
