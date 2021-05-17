9806fa456b12a644d43baaa6d6ce351aad5b5be1
Oleksii Sur <oleksii_sur@epam.com>
2021-05-17 21:44:04 +0300

    feat: solutions for exercises 4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,41 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+   
+    if (array.count == 0 || array == nil || ![array[0] isKindOfClass:NSArray.class]) {
+        return @[];
+    }
+    
+    NSMutableArray *reverseArray = [NSMutableArray array];
+    NSMutableArray *numbersArray = [NSMutableArray array];
+    NSMutableArray *stringsArray = [NSMutableArray array];
+    
+    for (int i = 0; i < array.count; i++) {
+        if ([array[i][0] isKindOfClass: NSNumber.class]) {
+            [numbersArray addObjectsFromArray: array[i]];
+        } else {
+            [stringsArray addObjectsFromArray: array[i]];
+        }
+    }
+    
+    if (numbersArray.count > 0) {
+        numbersArray = [[numbersArray sortedArrayUsingSelector:@selector(compare:)] mutableCopy];
+        if (stringsArray.count == 0) {
+            return [numbersArray copy];
+        }
+    }
+    
+    if (stringsArray.count > 0) {
+        stringsArray = [[stringsArray sortedArrayUsingSelector:@selector(compare:)] mutableCopy];
+        if (numbersArray.count == 0) {
+            return [stringsArray copy];
+        }
+    }
+    
+    [reverseArray addObject:[numbersArray copy]];
+    [reverseArray addObject:[[stringsArray reverseObjectEnumerator] allObjects]];
+    
+    return [reverseArray copy];
 }
 
 @end
